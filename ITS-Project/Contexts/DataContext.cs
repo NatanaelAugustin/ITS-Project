1a04c041340bc99b7a24026940345bef9a26e34a
Natanael Augustin <natanael.augustin@gmail.com>
2023-03-24 10:52:06 +0100

    Created folder structure and created and connected database to project.

---
+++ ITS-Project/Contexts/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ITS_Project.Contexts;
+
+internal class DataContext : DbContext
+{
+    public DataContext()
+    {
+
+    }
+
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\natan\Programmering\C#\Databas\ITS-Project\ITS-Project\Contexts\local-its-db.mdf;Integrated Security=True;Connect Timeout=30");
+    }
+}
