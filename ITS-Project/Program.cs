1547aa76a956e804f097769925af7f4daccc8799
Natanael Augustin <natanael.augustin@gmail.com>
2023-03-24 13:55:59 +0100

    Started bulding the Menu Service... to be continued

--- ITS-Project/Program.cs
+++ ITS-Project/Program.cs
@@ -1,5 +1,13 @@
 ﻿using ITS_Project.Services;
 
 StatusService statusService = new();
+MenuService menuService = new();
 
 await statusService.CreateStatusTypesAsync();
+
+while (true)
+{
+    Console.Clear();
+    await menuService.MainMenu();
+    Console.ReadKey();
+}
---
+++ ITS-Project/Services/MenuService.cs
@@ -0,0 +1,56 @@
+﻿namespace ITS_Project.Services;
+
+internal class MenuService
+{
+    private readonly UserService _userService = new();
+    private readonly CaseService _caseService = new();
+
+    public async Task MainMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("################## HuvudMeny ###############");
+        Console.WriteLine("1. Visa all aktiva arenden");
+        Console.WriteLine("2. Visa alla handlaggare");
+        Console.WriteLine("3. Skapa nytt arende");
+        Console.Write("valj ett av ovanstande alternativ");
+        var option = Console.ReadLine();
+
+        switch (option)
+        {
+            case "1":
+                await ActiveCasesAsync();
+                break;
+
+
+            case "2":
+                await HandlersAsync();
+                break;
+
+
+            case "3":
+                await NewCaseAsync();
+                break;
+
+            default:
+                break;
+
+        }
+    }
+
+    private async Task ActiveCasesAsync()
+    {
+
+    }
+
+    private async Task HandlersAsync()
+    {
+
+    }
+
+    private async Task NewCaseAsync()
+    {
+
+    }
+}
+}
+}
