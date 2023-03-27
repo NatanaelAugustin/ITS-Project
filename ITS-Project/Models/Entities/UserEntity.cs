e400fafe09ee3f6d1fe97b323b3e7ad1da93145a
Natanael Augustin <natanael.augustin@gmail.com>
2023-03-27 14:55:54 +0200

    updated UserEntity

--- ITS-Project/Models/Entities/UserEntity.cs
+++ ITS-Project/Models/Entities/UserEntity.cs
@@ -1,11 +1,9 @@
 ﻿namespace ITS_Project.Models.Entities;
 
-//[Index(nameof(Email), IsUnique = true)]
 internal class UserEntity
 {
     public int Id { get; set; }
-    public string FirstName { get; set; } = null!;
-    public string LastName { get; set; } = null!;
+    public string UserName { get; set; } = null!;
     public string Email { get; set; } = null!;
     public ICollection<CaseEntity> Cases { get; set; } = new List<CaseEntity>();
 }
