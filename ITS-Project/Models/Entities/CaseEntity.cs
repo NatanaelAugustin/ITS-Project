a53c810ba2cbeb007b9438345ebaf92aadb541f4
Natanael Augustin <natanael.augustin@gmail.com>
2023-03-27 14:52:16 +0200

    updated CaseEntity.cs

--- ITS-Project/Models/Entities/CaseEntity.cs
+++ ITS-Project/Models/Entities/CaseEntity.cs
@@ -3,9 +3,6 @@
 internal class CaseEntity
 {
     public int Id { get; set; }
-    public string CustomerName { get; set; } = null!;
-    public string CustomerEmail { get; set; } = null!;
-    public string CustomerPhoneNumber { get; set; } = null!;
     public string Description { get; set; } = null!;
     public DateTime Created { get; set; } = DateTime.Now;
     public DateTime Modified { get; set; } = DateTime.Now;
